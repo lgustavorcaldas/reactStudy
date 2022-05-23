c1a65d5ec71e28b08d1a1e737534a91cdf1f8ddf
Gustavo Caldas <lgustavorcaldas@gmail.ccom>
2022-05-23 18:11:36 -0300

    hey

--- reactProject/src/site/Header.jsx
+++ reactProject/src/site/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div>
-        <header>
-            <h1>Gerenciador de Tarefas - Kanban</h1>
-        </header>
-    </div>
-  )
+return (
+  <div>
+      <header>
+          <h1>Gerenciador de Tarefas - Kanban</h1>
+      </header>
+  </div>
+)
 }
