1e6a0d22e0e940bacd8b0cf6e83bf4d0bbc6e15c
Luiz Gustavo <lgustavorcaldas@gmail.com>
2022-05-22 22:25:47 -0300

    aiai

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -1,13 +1,11 @@
 import Inputs from "./site/Inputs";
 import Header from "./site/Header";
-import ToDoList from "./site/ToDoList";
 
 export function App() {
 	return (  
 		<div>
 			<Header />
 			<Inputs />
-			<ToDoList />
 		</div>
 	);
 }
--- reactProject/src/site/Inputs.jsx
+++ reactProject/src/site/Inputs.jsx
@@ -1,24 +1,66 @@
 import React, { useState } from 'react'
+import PostIt from './PostIt';
 
 export default function Inputs() {
-    const [ newTask, setNewTask ] = useState("");
-    const [ tasks, setTasks ] = useState(["Aula React", "Projeto Estágio", "Reunião Nuri"]);
+    const [ formTodo, setFormToDo ] = useState( [ {
+      yourTask: "Jogo do Corinthians",
+      date: "2022-05-18",
+      state: "toDo"
+    } ] );
+    
+    const [ newFormToDo, setNewFormToDo ] = useState( { yourTask: "", date: "", state: "" } );
 
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const auxFormToDo = formTodo;
+      auxFormToDo.push( newFormToDo );
+
+      setFormToDo( auxFormToDo );
+      setNewFormToDo( { yourTask: "", date: "", state: "" } );
+      console.log(formTodo);
+    }
     
 
   return (
     <div>
         <h2>Cadastrar uma Tarefa</h2>
         <div>
-            <input type="text" name="yourTask" id="yourTask" placeholder="Sua tarefa" />
-            <input type="date" name="term" id="term" placeholder="Sua tarefa" />
-            <select name="" id="">
+          <form onSubmit={handleSubmit} id="newFormToDo">
+            <input onChange={ ( e ) => setNewFormToDo( { ...newFormToDo, yourTask: e.target.value } ) } value={ newFormToDo.yourTask } type="text" name="yourTask" id="yourTask" placeholder="Sua tarefa" />
+            <input onChange={ ( e ) => setNewFormToDo( { ...newFormToDo, date: e.target.value } ) } value={ newFormToDo.date } type="date" name="date" id="date" />
+            <select onChange={ ( e ) => setNewFormToDo( { ...newFormToDo, state: e.target.value } ) } value={  newFormToDo.state } name="" id="" form='newFormToDo'>
+                <option value="none">--Escolha--</option>
                 <option value="toDo">A Fazer</option>
                 <option value="doing">Fazendo</option>
                 <option value="done">Finalizado</option>
             </select>
-            <input type="button" value="Inserir Tarefas" />
+            <input type="submit" value="Inserir Tarefas" />
+          </form>
         </div>
+      <section className='toDoList'>
+          <h2>Quadro de Tarefas</h2>
+          <div>
+              <div className='DivToDo'>
+                  <h3>A Fazer</h3>
+              {
+                <section className='teste'>
+                  {
+                    formTodo.map( ( auxPostIt, index ) => {
+                      console.log( auxPostIt );
+                        <PostIt desc={ "auxPostIt.yourTask" } date={ "auxPostIt.date" }  index={ index } key={ index } />
+                    })
+                  }
+                </section>
+              }
+              </div>
+              <div className='DivDoing'>
+                  <h3>Fazendo</h3>
+              </div>
+              <div className='DivDone'>
+                  <h3>Finalizado</h3>
+              </div>
+          </div>
+      </section>
     </div>
   )
 }
