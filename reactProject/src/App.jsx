496a592480daf0c27866e7d65dea3695c6ab3103
Gustavo Caldas <lgustavorcaldas@gmail.ccom>
2022-05-24 16:24:22 -0300

    hey

---
+++ reactProject/src/App.jsx
@@ -0,0 +1,11 @@
+import Inputs from "./site/Inputs";
+import Header from "./site/Header";
+
+export function App() {
+	return (  
+		<div className="container">
+			<Header />
+			<Inputs />
+		</div>
+	);
+}
---
+++ reactProject/src/site/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <header>
+            <h1>Gerenciador de Tarefas - Kanban</h1>
+        </header>
+    </div>
+  )
+}
